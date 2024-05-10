a578878bdda3b3f046f8ddc196aa7e7e8a510c1a
Simon Maass <simon.maass@meevo.de>
2024-05-10 16:09:27 +0200

    override addStockQty for ProductHelper class

--- Helper/InventoryProductHelper.php
+++ Helper/InventoryProductHelper.php
@@ -43,4 +43,61 @@ class InventoryProductHelper extends ProductHelper
         // Handled in ProductHelperPlugin
         return true;
     }
+
+    /**
+     * @param $defaultData
+     * @param $customData
+     * @param $additionalAttributes
+     * @param Product $product
+     * @return mixed
+     */
+    protected function addStockQty($defaultData, $customData, $additionalAttributes, Product $product)
+    {
+        if (isset($defaultData['stock_qty']) === false && $this->isAttributeEnabled($additionalAttributes, 'stock_qty')) {
+            $customData['stock_qty'] = 0;
+
+            $stockItem = $this->stockRegistry->getStockItem($product->getId());
+            if ($stockItem) {
+                $customData['stock_qty'] = (int)$stockItem->getQty();
+            }
+        }
+
+        if ($this->isMsiEnabled()) {
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            /** @var \Magento\InventorySalesApi\Model\StockByWebsiteIdResolverInterface $stockByWebsiteId */
+            $stockByWebsiteId = $objectManager->get('Magento\InventorySalesApi\Model\StockByWebsiteIdResolverInterface');
+            /** @var \Magento\Store\Model\StoreManagerInterface $storeManager */
+            $storeManager = $objectManager->get('Magento\Store\Model\StoreManagerInterface');
+            /** @var \Magento\InventorySalesApi\Api\GetProductSalableQtyInterface $getProductSalableQty */
+            $getProductSalableQty = $objectManager->get('Magento\InventorySalesApi\Api\GetProductSalableQtyInterface');
+
+            $stockId = 1;
+            $storeId = $product->getStoreId();
+            $websiteId = $storeManager->getStore($storeId)->getWebsiteId();
+            $stockId = $stockByWebsiteId->execute($websiteId)->getStockId();
+            $customData['stock_qty'] = $getProductSalableQty->execute($product['sku'], $stockId);
+        }
+
+        return $customData;
+    }
+
+    public function isMsiEnabled(): bool
+    {
+        // If Magento Inventory is not installed, no need for the external module
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        /** @var \Magento\Framework\Module\Manager $moduleManager */
+        $moduleManager = $objectManager->get('Magento\Framework\Module\Manager');
+        $hasMsiModule = $moduleManager->isEnabled('Magento_Inventory');
+        if (! $hasMsiModule) {
+            return false;
+        }
+
+        // Module installation is only needed if there's more than one source
+        $sourceCollection = $objectManager->get('\Magento\Inventory\Model\ResourceModel\Source\Collection');
+        if ($sourceCollection->getSize() <= 1) {
+            return false;
+        }
+
+        return true;
+    }
 }
